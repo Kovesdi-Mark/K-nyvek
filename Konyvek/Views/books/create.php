8f616df5caf89f516560db65d11990ce98fa75f3
Global User <nagy.zsolt2005@tanulo.boronkay.hu>
2025-10-06 14:02:46 +0200

    10.06 lenyíló lista

--- Konyvek/Views/books/create.php
+++ Konyvek/Views/books/create.php
@@ -5,12 +5,15 @@
     ISBN: <input type="text" name="isbn"><br>
     Price: <input type="number" name="price"><br>
     Description: <textarea name="description"></textarea><br>
-    Author: <select name="author_id">
+    Author: <select name="author_id" required>
+        <option value=""><?php echo "---válassz---"; ?></option>
         <?php foreach ($authors as $author): ?>
             <option value="<?php echo $author['id']; ?>"><?php echo $author['name']; ?></option>
         <?php endforeach; ?>
     </select><br>
     Publisher: <select name="publisher_id">
+        <option value=""><?php echo "---válassz---"; ?></option>
+
         <?php foreach ($publishers as $publisher): ?>
             <option value="<?php echo $publisher['id']; ?>"><?php echo $publisher['name']; ?></option>
         <?php endforeach; ?>
